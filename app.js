68ea0dd06c4329ad855c42bd50585eabce8484c2
garoweb262 <muhdgazzali01@gmail.com>
2024-06-17 12:02:48 +0100

    updates

--- app.js
+++ app.js
@@ -3,41 +3,31 @@ const express = require("express");
 const Sequelize = require("sequelize");
 const { sequelize } = require("./models");
 const app = express();
-var cors = require("cors");
-// var cookie = require("cookie");
+const cors = require("cors");
 const bodyParser = require("body-parser");
-// const nodemailer = require("nodemailer");
-// const path = require("path");
 const authRoute = require("./routes/auth");
 const staffRoute = require("./routes/staff");
 const applyRoute = require("./routes/apply");
-// const facultyRoute = require("./routes/faculty");
-// const departmentRoute = require("./routes/department");
-// const studentRoute = require("./routes/student");
-// const reportRoute = require("./routes/report");
-// const uploadRoute = require("./routes/upload");
-const Op = Sequelize.Op;
+const { endPoint } = require("./config/constant");
+
 let corsOptions = {
-  origin: "http://localhost:5009",
+  origin: "http://localhost:3002", // Update to your frontend origin
+  optionsSuccessStatus: 200,
 };
-app.use(cors());
-const {
-  endPoint,
-} = require("./config/constant");
 
+app.use(cors(corsOptions)); // Use the configured CORS options
+
+// Middleware
 app.use(express.json({ limit: "50mb" }));
-//body parser
-
 app.use(bodyParser.json());
 
-// cors
-
+// Static file serving
 app.use("/public", express.static("public"));
 app.use("/uploads/", express.static("uploads/"));
 
+// Database connectivity check
 app.get("/ping", async (req, res) => {
   try {
-    // Check database connection
     await sequelize.authenticate();
     console.log("Database connection has been established successfully.");
     res.json({
@@ -54,16 +44,14 @@ app.get("/ping", async (req, res) => {
     });
   }
 });
-const port = process.env.APP_PORT || 5009
+
 // Routes
 app.use(endPoint + "auth", authRoute);
 app.use(endPoint + "staff", staffRoute);
 app.use(endPoint + "apply", applyRoute);
-// app.use(endPoint + "faculty", facultyRoute);
-// app.use(endPoint + "department", departmentRoute);
-// app.use(endPoint + "student", studentRoute);
-// app.use(endPoint + "report", reportRoute);
-// app.use(endPoint + "upload", uploadRoute);
+
+// Server listen
+const port = process.env.APP_PORT || 5009;
 app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
