39f1fb19e1f5d8d8f9aded0503bbe0510241c7b3
nasredeenabdulhaleem <nabdulhaleem09@gmail.com>
2024-07-12 06:57:14 +0100

    heroku provisioning 3

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use(endPoint + "portal", portalRoute);
 app.use(endPoint + "approved", approvedRoute);
 
 // Server listen
-const port = process.env.APP_PORT || 5009;
+const port = process.env.APP_PORT || 5001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
